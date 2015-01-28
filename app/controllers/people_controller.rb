3e158c83d05a755f600062303add92eeb85a1212
CaseyKelly <casey.kelly@colorado.edu>
2015-01-28 15:19:35 -0700

    first commit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,17 @@ class PeopleController < ApplicationController
 
   def index
     @people = Person.all
+    if params[:first_name]
+      @people = Person.order(:first_name)
+    elsif params[:last_name]
+      @people = Person.order(:last_name)
+    elsif params[:eye_color]
+      @people = Person.order(:eye_color)
+    elsif params[:date_of_birth]
+      @people = Person.order(:date_of_birth)
+    elsif params[:awesome]
+      @people = Person.where(awesome: true)
+    end
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Person.create(first_name: 'Casey', last_name: 'Kelly', eye_color: 'blue', date_of_birth: '8/19/1988', height_in_inches: 74, awesome: true)
+Person.create(first_name: 'Shelby', last_name: 'Kelly', eye_color: 'blue', date_of_birth: '11/30/1990', height_in_inches: 55, awesome: false)
+Person.create(first_name: 'Andrew', last_name: 'James', eye_color: 'green', date_of_birth: '5/10/1988', height_in_inches: 68, awesome: true)
+Person.create(first_name: 'Caleb', last_name: 'Atwood', eye_color: 'green', date_of_birth: '5/10/1988', height_in_inches: 67, awesome: true)
+Person.create(first_name: 'Cameron', last_name: 'Webster', eye_color: 'green', date_of_birth: '5/10/1990', height_in_inches: 63, awesome: true)
+Person.create(first_name: 'Jess', last_name: 'Koch', eye_color: 'green', date_of_birth: '5/10/1984', height_in_inches: 60, awesome: true)
+Person.create(first_name: 'John', last_name: 'Marty', eye_color: 'green', date_of_birth: '5/10/2013', height_in_inches: 71, awesome: true)
+Person.create(first_name: 'Jake', last_name: 'Marwill', eye_color: 'green', date_of_birth: '5/10/2013', height_in_inches: 55, awesome: true)
+Person.create(first_name: 'Jordan', last_name: 'Collier', eye_color: 'green', date_of_birth: '5/10/2013', height_in_inches: 62, awesome: true)
